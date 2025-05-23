b94caf31fcdf67ea814246aaa81dddd718a195e6
Dipak Kumar Paul <pauldipakkr@gmail.com>
2025-05-23 13:04:59 +0530

    java codes

--- string.java
+++ string.java
@@ -6,10 +6,35 @@ public class string {
         System.out.println("size of the string " + e.toUpperCase()); // Convert to uppercase
         System.out.println("size of the string " + f.toLowerCase()); // Convert to lower case
 
+        // Java concatination
         String d = "Dipak";
         String k = "Kumar";
         String p = "Paul";
         System.out.println(d+ " " + k + " " +p);
+        // Ends
 
+        // Java Special Characters
+        String txt = "We are the so-called \"Vikings\" from the north.";
+        String txt1 = "We are the so-called \'Vikings\' from the north.";
+        String txt2 = "We are the so-called \\Vikings\\ from the north.";
+        System.out.println(txt);
+        System.out.println(txt1);
+        System.out.println(txt2);
+
+        // Other common escape sequences that are valid in Java are
+        String txt3 = "We are the so-called \n Vikings from the north.";
+        System.out.println(txt3);
+
+         String txt4 = "Hello \rVikings.";
+        System.out.println(txt4);
+
+        String txt5 = "We are the so-called \t Vikings from the north.";
+        System.out.println(txt5);
+
+         String txt6 = "We are the so-called \b Vikings from the north.";
+        System.out.println(txt6);
+
+        String txt7 = "We are the so-called \f Vikings from the north.";
+        System.out.println(txt7);
     }
 }
